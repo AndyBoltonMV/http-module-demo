fc6d40598dd18d7544ccfa9165e8842dc25ede4b
AndyBoltonMV <andrew.bolton@multiverse.io>
2023-03-15 11:17:46 +0000

    fixed declaration of resCode

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const endpoints = require("./User");
 
 const server = http.createServer(async (req, res) => {
   const resObj = {};
-  const resCode = 200;
+  let resCode = 200;
   if (endpoints[req.url][req.method]) {
     await endpoints[req.url][req.method](req, resCode, resObj);
     res.writeHead(resCode, { "Content-Type": "application/json" });
